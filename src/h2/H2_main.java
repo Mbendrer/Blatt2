e7e492b5cf4276a18bcaa22f21ebaa5b0a4b1966
Miriam <miriam@Miriams-MacBook-Pro.local>
2025-10-30 12:14:35 +0100

    erster commit

---
+++ src/h2/H2_main.java
@@ -0,0 +1,33 @@
+package h2;
+
+public class H2_main {
+	public static void main(String[] args) {
+
+		int i = 34343, j = 2, k = 18, min, max;
+		
+		if (i < j) {
+			min = i;
+		} else {
+			min = j;
+		}
+		
+		if (k < min) {
+			min = k;
+		}	
+			
+			
+		if (i > j) {
+			max = i;
+		} else {
+				max = j;
+		}
+			
+		if (k > max) {
+				max = k;	
+		}
+		
+		System.out.println("min ist: " + min);
+		System.out.println("max ist: " + max);
+
+	}	
+}
